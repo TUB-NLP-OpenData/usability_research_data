973de392b274b5d2f8c208fa0736a1f21dd45b4c
vwoloszyn <vinicius@open.inf.br>
2020-07-31 16:21:53 +0200

    add objects

--- redu/utils.py
+++ redu/utils.py
@@ -10,10 +10,49 @@ from tqdm import tqdm
 import urllib.request
 from itertools import islice
 from pandas_profiling import ProfileReport
-
+from urllib.parse import urlparse
+from os.path import splitext
 
 
 HOSTS=["https://depositonce.tu-berlin.de/"]
+
+def get_ext(url):
+    """Return the filename extension from url, or ''."""
+    parsed = urlparse(url)
+    root, ext = splitext(parsed.path)
+    return str(ext).lower()  # or ext[1:] if you don't want the leading '.'
+
+class Dataset():
+    def __init__(self):
+        self.title = None
+        self.author = None
+        self.id = None
+        self.url = None
+        self.abstract = None
+        self.content=None
+
+    def download(self):
+        s=requests.get(self.url).content
+        self.content=pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+    def preview(self):
+            extension=get_ext(self.url)
+            get_page = urllib.request.urlopen(self.url)
+            if extension==".csv":
+                return pd.read_csv(get_page, nrows=5).head()
+            elif extension==".json":
+                return pd.read_json(get_page, nrows=5).head()
+            else:
+                raise Exception("Sorry, filetype not suported") 
+
+    def df(self):
+        return self.content
+    
+    def describe(self):
+        get_page = urllib.request.urlopen('https://depositonce.tu-berlin.de/bitstream/' + id_ + '/2/Xb.csv')
+        df = pd.read_csv(get_page)
+        profile = ProfileReport(df, title='Pandas Profiling Report', explorative=True)
+        return profile
 
 
 class Element():
@@ -26,13 +65,16 @@ class Element():
         self.files=[]
 
     def to_dict(self):
-        return {"id":self.id,"title":self.title,"authors":self.title,"files":[f.split("/")[-1] for f in self.files],"url":self.url}
+        return {"id":self.id,"title":self.title,"authors":self.title,"files":[f.url.split("/")[-1] for f in self.files],"url":self.url}
+
+    def datasets(self):
+        return self.files
 
     def __str__(self):
         return str(self.title) + " - "+ str(len(self.files)) + " files"
 
 
-def get_element_from_handle(handle_id):
+def repository(handle_id):
     handle_id="/handle/"+handle_id.replace("/handle/","")
     url=HOSTS[-1] + handle_id
     #print (url)
@@ -46,12 +88,10 @@ def get_element_from_handle(handle_id):
         for div in soup.find_all('div', attrs={"class": "file-row"}):
             files= div.find_all('a', href=True)
             for f in files:
-                # print(f)
-                # print(HOSTS[-1])
-                # print(f["href"])
-                e.files.append(HOSTS[-1]+f["href"])
+                dataset= Dataset()
+                dataset.url=HOSTS[-1]+f["href"]
+                e.files.append(dataset)
     return e
-
 
 def get_elements_by_keywork(keyword, max_e=10):
 
@@ -65,7 +105,7 @@ def get_elements_by_keywork(keyword, max_e=10):
 
         for x in table:
             if len(x.find_all('a',href=True)) > 0:
-                elements.append(get_element_from_handle(x.find_all('a', href=True)[0]["href"]))
+                elements.append(repository(x.find_all('a', href=True)[0]["href"]))
         if soup.find('ul', attrs={"class": "pagination"}).find_all('li')[-1].find("a"):
             query_string = HOSTS[-1] + soup.find('ul', attrs={"class": "pagination"}).find_all('li')[-1].find("a")["href"]
         else:
@@ -78,26 +118,26 @@ def search(keyword, max_e=10):
     return pd.DataFrame(results)
 
 
-def get_datasets(id_,ind_=[-1]):
-    out=[]
-    files=[f for f in get_element_from_handle(id_).files]
-
-    for f in tqdm(files):
-        #print (f)
-        try:
-            s=requests.get(f).content
-            pdf=pd.read_csv(io.StringIO(s.decode('utf-8')))
-            out.append(pdf)
-        except:
-            pass
-    return out
+#def get_repo(id_,ind_=[-1]):
+#    out=[]
+#    files=[f for f in get_element_from_handle_id(id_).datasets]
+#
+#    for f in tqdm(files):
+#        #print (f)
+#        try:
+#            s=requests.get(f).content
+#            pdf=pd.read_csv(io.StringIO(s.decode('utf-8')))
+#            out.append(pdf)
+#        except:
+#            pass
+#    return out
 
 
 def get_preview(id_):
     #get_page = urllib.request.urlopen('https://depositonce.tu-berlin.de/bitstream/' + id_ + '/2/Xb.csv')
     #return pd.read_csv(get_page, nrows=5)
     out = []
-    files = [f for f in get_element_from_handle(id_).files]
+    files = [f for f in get_element_from_handle_id(id_).datasets]
     for f in tqdm(files):
         try:
             get_page = urllib.request.urlopen(f)
@@ -105,7 +145,6 @@ def get_preview(id_):
             out.append(var)
         except:
             pass
-
     return out
 
 
@@ -116,3 +155,6 @@ def describe(id_):
     df = pd.read_csv(get_page)
     profile = ProfileReport(df, title='Pandas Profiling Report', explorative=True)
     return profile
+
+
+
