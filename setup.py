757c1b230688ec2bd87b6ce3ea06b232c6edb4ee
Vinicius Woloszyn <vinicius@open.inf.br>
2020-10-09 11:39:46 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import pathlib
+
+import pkg_resources
+import setuptools
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+setuptools.setup(
+    install_requires=install_requires,
+)
