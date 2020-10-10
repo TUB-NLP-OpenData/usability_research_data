eec6b688bac2d0f7659a7fc21165311639d3cb8b
andrew_tristan <andrew.tristan@campus.tu-berlin.de>
2020-10-10 10:57:53 +0200

    bopi

---
+++ bopi/__init__.py
@@ -0,0 +1,12 @@
+from pkg_resources import get_distribution, DistributionNotFound
+from bop.utils import *
+
+#from redu.search import *
+
+
+HOSTS=["https://depositonce.tu-berlin.de/"]
+
+try:
+    __version__ = get_distribution('bopi').version
+except DistributionNotFound:
+    __version__ = '(local)'
